d04d4cb89a96a35f28b5a964fa879366391d5057
serloro <32571944+serloro@users.noreply.github.com>
2025-06-28 18:53:38 +0200

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,31 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add resolver configuration to handle potential module resolution issues
-config.resolver.platforms = ['native', 'web', 'ios', 'android'];
-
-// Increase timeout for Metro bundler
-config.server = {
-  ...config.server,
-  enhanceMiddleware: (middleware) => {
-    return (req, res, next) => {
-      // Increase timeout to prevent freezing
-      res.setTimeout(300000); // 5 minutes
-      return middleware(req, res, next);
-    };
-  },
-};
-
-// Configure transformer to handle large bundles
-config.transformer = {
-  ...config.transformer,
-  minifierConfig: {
-    // Reduce memory usage during minification
-    keep_fnames: true,
-    mangle: {
-      keep_fnames: true,
-    },
-  },
-};
-
 module.exports = config;
